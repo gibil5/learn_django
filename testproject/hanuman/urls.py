5eb14a537080a30d47cf140c1da6ccd697dc3458
gibil <jrevilla55@gmail.com>
2019-10-16 20:14:47 -0500

    ..

--- testproject/hanuman/urls.py
+++ testproject/hanuman/urls.py
@@ -11,7 +11,9 @@ urlpatterns = [
 
 				url(r'^$', views.index, name='index'),
 
-				#url(r'^(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
+				url(r'^(?P<programmer_id>[0-9]+)/$', views.detail, name='detail'),
+				
+
 				#url(r'^(?P<question_id>[0-9]+)/results$', views.results, name='results'),
 				#url(r'^(?P<question_id>[0-9]+)/vote$', views.vote, name='vote'),
 
--- testproject/hanuman/views.py
+++ testproject/hanuman/views.py
@@ -7,6 +7,9 @@ from django.utils import timezone
 from django.http import HttpResponse
 
 
+from django.shortcuts import render, get_object_or_404
+
+
 # Create your views here.
 
 from .models import Programmer
@@ -21,15 +24,38 @@ def index(request):
 	#return HttpResponse("Hello World ! You are at the Hanuman index page.")
 
 
-	latest_programmer_list = Programmer.objects.order_by('-pub_date')[:5]
-	
+	#latest_programmer_list = Programmer.objects.order_by('-pub_date')[:5]
+	latest_programmer_list = Programmer.objects.all()
 	print(latest_programmer_list)
 
 	
 	ctx = {'latest_programmer_list': latest_programmer_list,}
 	
-	output = render(request, 'hanuman/index.html', ctx)
-
 	#output = 'output'
+	output = render(request, 'hanuman/index.html', ctx)
+	print(output)
+
+
 	return HttpResponse(output)
+
+
+
+def detail(request, programmer_id):
+	print()
+	print('Hanu - Detail')
+
+
+	# Get Object
+	programmer = get_object_or_404(Programmer, pk=programmer_id)  		# Shortcut !
+
+	print(programmer)
+
+
+	ctx = {'programmer': programmer,}
+
+	return	render(request, 'hanuman/detail.html', ctx)
+
+
+
+
 
